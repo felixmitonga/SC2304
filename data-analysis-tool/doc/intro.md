05b97d1f5ae8f82db5f70af005afb7dcd7d36a5e
Felix Mitonga <63664755+felixmitonga@users.noreply.github.com>
2024-01-19 22:44:18 +0100

    Delete data-analysis-tool directory

--- data-analysis-tool/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to data-analysis-tool
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- data-analysis-tool/test/data_analysis_tool/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns data-analysis-tool.core-test
-  (:require [clojure.test :refer :all]
-            [data-analysis-tool.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
